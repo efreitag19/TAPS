13efd1179e93e0341b93d3e334fb7f2f574bfbc0
efreitag19 <eloisewf@gmail.com>
2025-05-21 12:03:12 -0400

    Update eftools.R

--- eftools.R
+++ eftools.R
@@ -1,16 +1,16 @@
 # eftools.R - Loads all functions from TAPS repository
 
 # Source individual function files
-source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/snv.R")
-source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/qual.R")
-source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/plots.R")
-source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/functions.R")
+source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/snv.R")
+source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/qual.R")
+source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/plots.R")
+source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/functions.R")
 
 load_eftools <- function() {
-  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/snv.R")
-  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/qual.R")
-  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/plots.R")
-  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/functions/functions.R")
+  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/snv.R")
+  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/qual.R")
+  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/plots.R")
+  source("/gpfs/data/imielinskilab/home/freite01/git/TAPS/R/functions.R")
   
   cat("All eftools functions loaded successfully!\n")
 }
